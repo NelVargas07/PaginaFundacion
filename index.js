3feef7e83a9e42a99bf4ba4c5e826981170c60db
Miguel-Araya <miguelarayaarroyo12345@gmail.com>
2024-10-06 10:45:52 -0600

    Arreglar el despliegue del modal contacto

--- index.js
+++ index.js
@@ -28,26 +28,41 @@ swiper.on('slideChange', () => {
 // Establece el radio button inicial como activo
 radios[swiper.activeIndex].checked = true; // Asegúrate de que el radio inicial esté marcado
 
-// Inicialización de Swiper
 document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper('.swiper-container', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-
+    
     //Inicio Implementacion de los modales
     const modalButtons = document.querySelectorAll('.modal-button');
     const closeButtons = document.querySelectorAll('.close');
     const modals = document.querySelectorAll('.modal');
 
     modalButtons.forEach(button => {
+
         button.addEventListener('click', () => {
+
+            //no deja que coexitan varios modales a la vez cuando se presiona otra opcion
+            // document.querySelectorAll('.show').forEach((element) => {
+            //     element.classList.remove('show');
+            // });
+
             const target = document.querySelector(button.getAttribute('data-target'));
-            target.classList.add('show');
+            
+            const modalContent = target.querySelector('.modal-content'); // Asegúrate de seleccionar el contenido del modal
+
+            //Para que no quede tan pegado a la parte derecha de la pantalla
+            //cuando se deba desplegar en esa posicion
+            const marginModal = 10;
+            
+            const buttonRect = button.getBoundingClientRect();
+
+            target.style.display = 'block';
+            
+            //Manejar las posiciones del modal para que no se salga de la pantalla y este colocado donde se presiona el boton
+            const positionModal = buttonRect.x + modalContent.offsetWidth > window.innerWidth ? (buttonRect.x - (buttonRect.x + ((modalContent.offsetWidth/2) - window.innerWidth))) - marginModal  : buttonRect.x + modalContent.offsetWidth/2;
+            
             setTimeout(() => {
-                target.style.display = 'block';
+                modalContent.style.left = `${positionModal}px`;
+
+                target.classList.add('show');    
             }, 100); // Permite que la clase `show` tome efecto antes de cambiar el display
         });
     });
@@ -65,11 +80,19 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('click', (event) => {
         if (event.target.classList.contains('modal')) {
             event.target.classList.remove('show');
+
+            //desaparecer todos los modales cuando se presiona en la pantalla, pero
+            //pueden coexistir varios mientra no se presione en la pantalla
+
+            // document.querySelectorAll('.show').forEach((element) => {
+            //     element.classList.remove('show');
+            // });
+
             setTimeout(() => {
                 event.target.style.display = 'none';
             }, 500); // Esperar a que termine la transición
         }
+
     });
     //Fin de la implementacion de los modales
-
 });
