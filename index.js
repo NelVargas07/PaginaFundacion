5fa755bd9782781b6fe4f9c3b95e527fcd84434e
NelVargas07 <vargascalderonnelson@gmail.com>
2024-10-06 10:33:39 -0600

    modal

--- index.js
+++ index.js
@@ -27,3 +27,49 @@ swiper.on('slideChange', () => {
 
 // Establece el radio button inicial como activo
 radios[swiper.activeIndex].checked = true; // Asegúrate de que el radio inicial esté marcado
+
+// Inicialización de Swiper
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    //Inicio Implementacion de los modales
+    const modalButtons = document.querySelectorAll('.modal-button');
+    const closeButtons = document.querySelectorAll('.close');
+    const modals = document.querySelectorAll('.modal');
+
+    modalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = document.querySelector(button.getAttribute('data-target'));
+            target.classList.add('show');
+            setTimeout(() => {
+                target.style.display = 'block';
+            }, 100); // Permite que la clase `show` tome efecto antes de cambiar el display
+        });
+    });
+
+    closeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const target = button.closest('.modal');
+            target.classList.remove('show');
+            setTimeout(() => {
+                target.style.display = 'none';
+            }, 500); // Esperar a que termine la transición
+        });
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target.classList.contains('modal')) {
+            event.target.classList.remove('show');
+            setTimeout(() => {
+                event.target.style.display = 'none';
+            }, 500); // Esperar a que termine la transición
+        }
+    });
+    //Fin de la implementacion de los modales
+
+});
